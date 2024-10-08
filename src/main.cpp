a491b46c983a3d5b22f1feca125e723e3d4f616b
Victoria Jury <victoriaehodges@gmail.com>
2024-10-08 09:48:49 +0200

    refactor main, add test mode and test data json file

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <fstream>
 #include <string>
-#include <map>
-#include <sqlite3.h>
 
 #include "../include/api.h"
+// #include "../include/database.h"
 
 double convertCurrency(double &base_amount, double &rate);
+void error_message(std::string err_msg);
 
 int main()
 {
+    bool is_test_mode = true;
+
     std::string api_key;
     std::string response;
     double base_amount;
@@ -17,61 +20,82 @@ int main()
 
     const std::string BASE_CURRENCY = "GBP";
     const std::vector<std::string> CURRENCY_LIST = {"EUR", "CHF", "USD"};
+    // const std::vector<std::string> CURRENCY_LIST = {};
 
-    /* TEST DATA & API URL */
-    /*
-    const std::map<std::string, double> test_data = {
-            {"EUR", 1.19616},
-            {"CHF", 1.12681},
-            {"USD", 1.31246}
-        };
-    */
-    // std::string api_url = "http://date.jsontest.com/";
+    // ----- Check API Key is set -----
+    if (!isSetAPIKey("CC_API_KEY", api_key))
+    {
+        error_message("API key not set. Run script: source ./set_api_key.sh");
+        return 0;
+    }
 
+    // ----- Initialise program -----
+    std::cout << "Currency Converter Initialized" << std::endl;
+    std::cout << "==============================" << std::endl;
+    std::cout << "Enter the amount to convert (" << BASE_CURRENCY << "):" << std::endl;
+    std::cin >> base_amount;
+    std::cout << "==============================" << std::endl;
 
-    if (isSetAPIKey("CC_API_KEY", api_key)) {
-        std::cout << "Currency Converter Initialized" << std::endl;
-        std::cout << "==============================" << std::endl;
-
-        std::cout << "Enter the amount to convert (" << BASE_CURRENCY << "):" << std::endl;
-        std::cin >> base_amount;
-
-        std::cout << "==============================" << std::endl;
- 
+    // ----- Call API -----
+    if (is_test_mode) // to save on API calls
+    {
+        // get data from test.json
+        std::cout << "Getting data from test file..." << std::endl;
+        std::ifstream jsonFile("../data/test.json");
+        std::ostringstream tmp;
+        tmp << jsonFile.rdbuf();
+        response = tmp.str();
+        jsonFile.close();
+    }
+    else
+    {
+        // get data from API
         std::string api_url = "https://api.currencyapi.com/v3/latest?apikey=" + api_key + "&base_currency=" + BASE_CURRENCY;
         curl_global_init(CURL_GLOBAL_DEFAULT);
-
-        if (performCurlRequest(api_url, response))
+        if (!performCurlRequest(api_url, response))
         {
-            Json::Value root;
-            if (parseJsonResponse(response, root))
-            {
-                // TEST: std::cout << root << std::endl;
-                const Json::Value rates = root["data"];
-                const Json::Value last_update = root["meta"]["last_updated_at"];
-                std::cout << "Last updated: " << last_update << std::endl;
-                std::cout << base_amount << ' ' << BASE_CURRENCY << " returns: " << std::endl;
-                for (std::string curr : CURRENCY_LIST)
-                {
-                    rate = rates[curr]["value"].asDouble();
-                    //TEST: rate = test_data.at(curr);
-
-                    converted_amount = convertCurrency(base_amount, rate);
-                    std::cout << "  " << converted_amount << ' ' << curr << std::endl;
-                }
-            }
+            return 0;
         }
-
         curl_global_cleanup();
     }
-    else {
-        std::cout << "------------- ERROR! -------------" << std::endl;
-        std::cout << "Currency Converter API key not set" << std::endl;
-        std::cout << "----------------------------------" << std::endl;
+
+    // ----- Parse JSON data -----
+    Json::Value root;
+    if (parseJsonResponse(response, root))
+    {
+        const Json::Value rates = root["data"];
+        const Json::Value last_update = root["meta"]["last_updated_at"];
+        std::vector<std::string> currencies;
+
+        // TODO:
+
+        // 1. Set up database and tables, if none exists
+        // DAILY UPDATES
+        // 2. Create table columns for each currency name
+        // 3. Populate rows for each timestamp since last updated, if time > 24hrs since last fetch
+
+        if (CURRENCY_LIST.size() > 0)
+        {
+            currencies = CURRENCY_LIST;
+        }
+        else
+        {
+            currencies = rates.getMemberNames();
+        }
+
+        // ----- Print converted currency data -----
+        std::cout << "Last updated: " << last_update << std::endl;
+        std::cout << base_amount << ' ' << BASE_CURRENCY << " returns: " << std::endl;
+        for (std::string curr : currencies)
+        {
+            rate = rates[curr]["value"].asDouble();
+            converted_amount = convertCurrency(base_amount, rate);
+            std::cout << "  " << converted_amount << ' ' << curr << std::endl;
+        }
     }
+
     return 0;
 }
-
 
 double convertCurrency(double &base_amount, double &rate)
 {
@@ -80,4 +104,11 @@ double convertCurrency(double &base_amount, double &rate)
     converted_amount = base_amount * rate;
 
     return converted_amount;
+}
+
+void error_message(std::string err_msg)
+{
+    std::cerr << "------------- ERROR! -------------" << std::endl;
+    std::cerr << err_msg << std::endl;
+    std::cerr << "----------------------------------" << std::endl;
 }
